9e7b8dd48b3d2a2162ad0a777bb2664c04671c19
Adam McGurk <amcgurk@bamboohr.com>
2021-07-27 16:14:44 -0600

    All dataobject stuff is done

---
+++ tests/units/ShinePHPTest/DataObjects/BaseDataObjectTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace ShinePHPTest\DataObjects;
+use PHPUnit\Framework\TestCase;
+use ShinePHPTest\TestDataObjects\BaseTestDataObject;
+
+final class BaseDataObjectTest extends TestCase {
+
+	public function testCreationOfBasicDataObject(): void {
+		$testDataObject = new BaseTestDataObject(['id' => 123]);
+		$this->assertEquals($testDataObject->id, 123);
+	}
+
+	public function testCannotCreateEmptyDataObject(): void {
+		$this->expectException(\InvalidArgumentException::class);
+		new BaseTestDataObject([]);
+	}
+
+	public function testCanCreateDataObjectWithExtraData(): void {
+		$testDataObject = new BaseTestDataObject(['id' => 123, 'name' => 'Adam McGurk']);
+		$this->assertEquals($testDataObject->id, 123);
+		$this->assertNull($testDataObject->name);
+	}
+
+}
---
+++ tests/units/ShinePHPTest/TestDataObjects/BaseTestDataObject.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ShinePHPTest\TestDataObjects;
+
+use ShinePHP\DataObjects\BaseDataObject;
+
+class BaseTestDataObject extends BaseDataObject {
+	public $id;
+}
